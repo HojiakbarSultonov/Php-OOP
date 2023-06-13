c362aa52b0aac0da61e8a09e7245cfbd3ca6664f
HojiakbarSultonov <hojiakbarrasulovich@gmail.com>
2023-06-13 09:58:09 +0500

    __construct

--- index.php
+++ index.php
@@ -1,20 +1,39 @@
 <?php
+//
+//class Car {
+//    public $model;
+//    public $year;
+//    public function drive(){
+//        return 'driving';
+//    }
+//    public function getFuel(){
+//        return 'fuelling';
+//    }
+////    public function setModal(){
+////        $this->model = $model;
+////    }
+//}
+//
+//
+//$bmw = new Car(); // Yangi object yaratish uchun new kalit so'zi bn!!!
+//
+////$bmw ->setModal('Bmw');
+//$bmw ->year = 2023;
 
-class Car {
-    public $model;
-    public $year;
-    public function drive(){
-        return 'driving';
-    }
-    public function getFuel(){
-        return 'fuelling';
-    } public function setModal(){
-        $this->model = $model;
+class Product {
+    public $name;
+    public $weight;
+
+
+    public function __construct($name, $weight )
+    {
+        $this->name = $name;
+        $this->weight = $weight;
     }
 }
 
 
-$bmw = new Car(); // Yangi object yaratish uchun new kalit so'zi bn!!!
+$prod = new Product('Apple', '100gr');
 
-$bmw ->setModal('Bmw');
-$bmw ->year = 2023;
+echo ($prod->name);
+echo ($prod->weight);
